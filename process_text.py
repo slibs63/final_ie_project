9213ab153f38c9f7b21206d21cf2b5ce700e9f0c
slibs63 <sara.libenson@gmail.com>
2015-04-27 08:41:03 -0400

    Break up chapters by paragraph

--- process_text.py
+++ process_text.py
@@ -1,21 +1,32 @@
 import os
 import re
-import json
 import nltk
 
 def save_chapters(chapters):
+    """
+    Save each chapter in a list to its own separate file.
+    """
     path = os.getcwd() + '/data/chapters/chapter_'
     for i, chapter in enumerate(chapters):
         with open(path + str(i) + '.txt', 'w') as outfile:
-            json.dump(chapter, outfile)
+            for paragraph in chapter:
+                outfile.write(paragraph + '\n')
 
 def preprocess_text(chapter):
+    """
+    Perform sentence tokenization, word tokenization, and part-of-speech tagging
+    on a chapter.
+    """
     chapter = [[nltk.pos_tag(nltk.word_tokenize(sent)) for sent in
                 nltk.sent_tokenize(paragraph)]
                for paragraph in chapter]
     return chapter
 
 def write_to_file(chapter, i):
+    """
+    Given a processed (tokenized and tagged) chapter and its corresponding
+    number, save it as a file.
+    """
     path = os.getcwd() + '/data/pos_tagged/chapter_'
     with open(path + str(i) + '.pos', 'w') as outfile:
         for paragraph in chapter:
@@ -25,6 +36,9 @@ def write_to_file(chapter, i):
             outfile.write(string + '\n')
 
 def read_file():
+    """
+    Read raw text file, and split it up into paragraphs.
+    """
     with open(os.getcwd() + '/data/game_of_thrones.txt', 'r') as infile:
         raw_text = infile.read()
 
@@ -34,6 +48,9 @@ def read_file():
     return paras_to_chapters(paragraphs)
 
 def paras_to_chapters(paragraphs):
+    """
+    Take paragraphs from raw text file and organize them into distinct chapters.
+    """
     chapters = []
     chapter = [paragraphs[0]]
     for paragraph in paragraphs[1:]:
